ed427c74d544ba52087bcf44677d9b7cfc9cef42
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-06-17 18:52:51 +0545

    feat:upd login signup position

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 };
 function SignupPage() {
   return (
-    <div className="flex justify-center mt-[10%] backdrop-blur-3xl  ">
+    <div className="flex justify-center mt-[5%] backdrop-blur-3xl  ">
       <div className="w-[500px] bg-white bg-opacity-75 p-6 rounded-lg ">
         <SignupForm />
       </div>
