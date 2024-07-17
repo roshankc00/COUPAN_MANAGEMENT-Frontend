21283cd1e7a7cc56cc6c99da791c8b1611920a15
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-07-17 16:09:42 +0545

    added the ogimage and metadata for product

--- app/(userroute)/user/products/[productId]/page.tsx
+++ app/(userroute)/user/products/[productId]/page.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import Product from "./_components/Product";
 import { getSingleProduct } from "@/common/api/products/products.api";
+import { Metadata, ResolvingMetadata } from "next";
 
-export const metadata = {
-  title: "Product | NepQue ",
-  description: "NepQue: Your CouponPartner",
-};
+// export const metadata = {
+//   title: "Product | NepQue ",
+//   description: "NepQue: Your CouponPartner",
+// };
+
+export async function generateMetadata(
+  { params }: { params: { productId: number } },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const id = params.productId;
+
+  // fetch data
+  const product = await fetch(
+    `https://api.nepque.com/api/v1/products/${id}`
+  ).then((res) => res.json());
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  const tags = product?.tags;
+  let keywords = ["product"];
+  if (tags && tags.length > 0) {
+    keywords = [...keywords, ...tags];
+  }
+  return {
+    title: product.title,
+    description: product?.description,
+    openGraph: {
+      images: [product?.imageUrl, ...previousImages],
+    },
+    keywords,
+  };
+}
+
 const ProductPage = ({ params }: { params: { productId: number } }) => {
   return (
     <div>
