5e18326dbd0dd8348f484ce40ca3cfc43ee46605
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-07-17 14:28:36 +0545

    fix:fixed the build issue

--- app/(userroute)/user/blogs/_components/AllBlogs.tsx
+++ app/(userroute)/user/blogs/_components/AllBlogs.tsx
@@ -1,69 +1,70 @@
-"use client";
-import { Card, CardDescription, CardTitle } from "@/components/ui/card";
-import { UseGetAllBlogs } from "@/hooks/react-query/blogs/get-all-blogs.hook";
-import debounce from "lodash.debounce";
-import React, { useCallback, useEffect } from "react";
-import Pagination, { usePagination } from "@/components/ui/pagination";
-import BlogSkeleton from "@/components/BlogSkeleton";
+// "use client";
+// import { Card, CardDescription, CardTitle } from "@/components/ui/card";
+// import { UseGetAllBlogs } from "@/hooks/react-query/blogs/get-all-blogs.hook";
+// import debounce from "lodash.debounce";
+// import React, { useCallback, useEffect } from "react";
+// import Pagination, { usePagination } from "@/components/ui/pagination";
+// import BlogSkeleton from "@/components/BlogSkeleton";
 
-const AllBlogs = () => {
-  const paginationProps = usePagination();
-  const { data, isLoading, isFetching, refetch } = UseGetAllBlogs(
-    paginationProps.currentPage,
-    10
-  );
-  const onSubmit = () => refetch();
+// const AllBlogs = () => {
+//   const paginationProps = usePagination();
+//   const { data, isLoading, isFetching, refetch } = UseGetAllBlogs(
+//     paginationProps.currentPage,
+//     10
+//   );
+//   const onSubmit = () => refetch();
 
-  const debouncedSubmit = debounce(onSubmit, 400);
-  const _debounceSubmit = useCallback(debouncedSubmit, []);
+//   const debouncedSubmit = debounce(onSubmit, 400);
+//   const _debounceSubmit = useCallback(debouncedSubmit, []);
 
-  useEffect(() => {
-    _debounceSubmit();
-  }, [paginationProps.currentPage]);
-  return (
-    <div>
-      <h1 className="my-10 mt-10 text-3xl font-medium text-center">
-        Our Blogs
-      </h1>
-      <div className="grid grid-cols-3 gap-4 gap-y-4 mt-20">
-        {!isLoading &&
-          !isFetching &&
-          data?.blogs?.map((item: any) => (
-            <div key={item.id}>
-              <Card className="p-3 rounded-md shadow-sm">
-                <CardTitle>
-                  <img
-                    src={`${process.env.NEXT_PUBLIC_SERVER_URL}/images/${item?.thumbnail}`}
-                    alt=""
-                    className="w-full h-full"
-                  />
-                </CardTitle>
-                <CardDescription className="px-4">
-                  <div>
-                    <div className="flex gap-3 flex-wrap mb-2">
-                      <h5 className="text-blue-600">Free Shipping</h5>
-                      <h5>11 min read</h5>
-                    </div>
-                    <h1 className="text-xl text-black">{item?.title}</h1>
-                  </div>
-                </CardDescription>
-              </Card>
-            </div>
-          ))}
+//   useEffect(() => {
+//     _debounceSubmit();
+//   }, [paginationProps.currentPage]);
+//   return (
+//     <div>
+//       <h1 className="my-10 mt-10 text-3xl font-medium text-center">
+//         Our Blogs
+//       </h1>
+//       <div className="grid grid-cols-3 gap-4 gap-y-4 mt-20">
+//         {!isLoading &&
+//           !isFetching &&
+//           data?.blogs?.map((item: any) => (
+//             <div key={item.id}>
+//               <Card className="p-3 rounded-md shadow-sm">
+//                 <CardTitle>
+//                   <img
+//                     src={`${process.env.NEXT_PUBLIC_SERVER_URL}/images/${item?.thumbnail}`}
+//                     alt=""
+//                     className="w-full h-full"
+//                   />
+//                 </CardTitle>
+//                 <CardDescription className="px-4">
+//                   <div>
+//                     <div className="flex gap-3 flex-wrap mb-2">
+//                       <h5 className="text-blue-600">Free Shipping</h5>
+//                       <h5>11 min read</h5>
+//                     </div>
+//                     <h1 className="text-xl text-black">{item?.title}</h1>
+//                   </div>
+//                 </CardDescription>
+//               </Card>
+//             </div>
+//           ))}
 
-        {isLoading &&
-          isFetching &&
-          new Array(12)
-            .fill(null)
-            .map((el, index) => <BlogSkeleton key={index} />)}
-      </div>
-      <div className="flex justify-center mt-10">
-        {!isFetching && !isLoading && data?.totalPage && (
-          <Pagination {...paginationProps} totalPages={data?.totalPage} />
-        )}
-      </div>
-    </div>
-  );
-};
+//         {isLoading &&
+//           isFetching &&
+//           new Array(12)
+//             .fill(null)
+//             .map((el, index) => <BlogSkeleton key={index} />)}
+//       </div>
+//       <div className="flex justify-center mt-10">
+//         {!isFetching && !isLoading && data?.totalPage && (
+//           <Pagination {...paginationProps} totalPages={data?.totalPage} />
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
 
-export default AllBlogs;
+// export default AllBlogs;
+
--- app/(userroute)/user/blogs/page.tsx
+++ app/(userroute)/user/blogs/page.tsx
@@ -1,13 +1,21 @@
-import { Card, CardTitle } from "@/components/ui/card";
-import React from "react";
-import AllBlogs from "../../../(userroute)/user/blogs/_components/AllBlogs";
+// import { Card, CardTitle } from "@/components/ui/card";
+// import React from "react";
+// import AllBlogs from "../../../(userroute)/user/blogs/_components/AllBlogs";
 
-const Blogs = () => {
-  return (
-    <main className="mx-auto max-w-6xl px-4 sm:px-6 lg-px-8">
-      <AllBlogs />
-    </main>
-  );
+// const Blogs = () => {
+//   return (
+//     <main className="mx-auto max-w-6xl px-4 sm:px-6 lg-px-8">
+//       <AllBlogs />
+//     </main>
+//   );
+// };
+
+// export default Blogs;
+
+import React from "react";
+
+const BlogsPage = () => {
+  return <div>Comming soon</div>;
 };
 
-export default Blogs;
+export default BlogsPage;
