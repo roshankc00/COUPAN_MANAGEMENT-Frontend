e9d622f96efb66140895329068c00eb26838406e
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2024-07-21 17:23:24 +0545

    fix:search modal in small screen

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -52,7 +52,7 @@ const SearchBar = () => {
         </DialogTrigger>
         <DialogContent className=" ml-0 xl:-ml-8   w-[100vw]">
           <div>
-            <div className="relative mt-[150px]  sm:mt-3">
+            <div className="relative mt-[200px]  sm:mt-3">
               <Input
                 value={searchText}
                 onChange={(e) => {
